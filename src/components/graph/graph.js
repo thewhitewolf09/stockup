335c1ac9a6e8bd4531bf8f745069a5da3a0967d1
thewhitewolf09 <sumitramprakashnirmal@gmail.com>
2022-10-30 19:14:55 +0530

    first commit

--- src/components/graph/graph.js
+++ src/components/graph/graph.js
@@ -1,34 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./graph.css"
 import { Chart } from "react-google-charts";
 
 
 const Graph = () => {
 
-    var data = [
-        ["Time", "Price"],
-        ["2004", 1000],
-        ["2005", 1170],
-        ["2006", 660],
-        ["2007", 1030],
-        ["2004", 1000],
-        ["2005", 1170],
-        ["2006", 660],
-        ["2007", 1030],
-        ["2012", 1000]
-        
-    ];
+    // var data = [
+    //     ["Time", "Price"],
+    //     ["2004", 1000],
+    //     ["2005", 1170],
+    //     ["2006", 660],
+    //     ["2007", 1030],
+    //     ["2004", 1000],
+    //     ["2005", 1170],
+    //     ["2006", 660],
+    //     ["2007", 1030],
+    //     ["2012", 1000]
 
+    // ];
+
+
+    const [graphdata, setGraphdata] = useState();
+
+
+
+    const getGraphData = async () => {
+        const res = await fetch("http://localhost:5000/api/v1/marketdata ", {
+            method: "GET",
+            headers: { "Content-Type": "application/json" }
+        })
+        const res_data = await res.json();
+        setGraphdata(res_data.market_data);
+    }
+    useEffect(() => {
+        getGraphData();
+    });
     const options = {
         title: "Market Price Chart",
         legend: { position: "none" },
     };
+
+    // console.log(graphdata)
+    var data = [['Time', 'Price']];
+
+    graphdata && graphdata.map((Item) => data.push([Item.date, Item.price]))
+
     return (
         <div className="graph-container">
             <div className='graph-heading'>
                 <div>
                     <span>Asset</span>
-                    <span>Price : ₹100.4587</span>
+                    <span>Price : ₹{graphdata && graphdata.reverse()[0].price}</span>
                 </div>
             </div>
             <div>
